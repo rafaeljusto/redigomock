4e0507ff27f227d0cb583506b17e0571d2c94bf7
Arnaud Rebillout <arnaudr@kali.org>
2025-06-05 11:02:19 +0700

    Update license GPL -> MIT in .go files too
    License was changed from GPL to MIT in
    8b8b83f41892e391799562cf9bd9efe754c8383f, however some files still
    retain the wording "this source code is governed by a GPL license".
    
    Update it there as well to clear the confusion.

--- command.go
+++ command.go
@@ -1,5 +1,5 @@
 // Copyright 2014 Rafael Dantas Justo. All rights reserved.
-// Use of this source code is governed by a GPL
+// Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
 package redigomock
--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 // Copyright 2014 Rafael Dantas Justo. All rights reserved.
 
-// Use of this source code is governed by a GPL
+// Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
 // Package redigomock is a mock for redigo library (redis client)
